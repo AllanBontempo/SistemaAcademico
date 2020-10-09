0c92c41e29672a7b10e5666c9d74d5bd106e746f
Allan Bontempo <allanbt@hotmail.com>
2020-10-09 19:02:03 -0300

    trabalho terminado

--- SistemaPet/sistemaPet.cpp
+++ SistemaPet/sistemaPet.cpp
@@ -53,7 +53,10 @@ Pet cadastrarPet(string nome, string porte, string especie,
                  int posicaoVagaDono, Donos *donos);
 int pesquisaIdPet(Pet *pets, int posicaoVagaPet, int idPet);
 void mostrarPets(int posicaoVagaPet, Pet *pets, Donos *donos, int idDono);
-
+void mostrarRelatorioPet(Cadastro *cadastro, Pet *pets, int idPet, int posicaoVagaCadastro);
+void mostrarTodosRelatorios(Cadastro *cadastro, Pet *pets, int posicaoVagaCadastro);
+Cadastro cadastrarRelatorio(string queixa, string diagnostico, string tratamento, string dataHora, int idPet, int id);
+int pesquisaIdRelatorio(Cadastro *cadastro, int posicaoVagaCadastro, int idCadastro);
 
 int main()
 {
@@ -74,6 +77,10 @@ int main()
     string porte;
     string especie;
     int idade;
+    string queixa;
+    string diagnostico;
+    string dataHora;
+    string tratamento;
     char sexo;
     float peso;
     string nome;
@@ -123,7 +130,7 @@ int main()
     idCadastro++;
     posicaoVagaCadastro++;
 
-    cadastros[posicaoVagaCadastro].dataHora = "10/05/2019";
+    cadastros[posicaoVagaCadastro].dataHora = "10/05/2019 as 17:00";
     cadastros[posicaoVagaCadastro].diagnostico = "Pet quebrou a pata";
     cadastros[posicaoVagaCadastro].queixa = "Pet com dor";
     cadastros[posicaoVagaCadastro].idPet = 2;
@@ -177,6 +184,7 @@ int main()
 
                     system("cls");
                     cout << "ALTERAR DADOS DO DONO\n\n";
+                    cout << "Digite o id do Dono:\n";
                     pesquisaId = procurarID(idDono);
 
                     pesquisaBemSucedida = pesquisarIdDonos(donos, posicaoVagaDono, pesquisaId);
@@ -211,6 +219,7 @@ int main()
                 {
 
                     cout << "EXCLUIR DADOS DO DONO\n\n";
+                    cout << "Digite o id do Dono:\n";
                     pesquisaId = procurarID(idDono);
                     pesquisaBemSucedida = pesquisarIdDonos(donos, posicaoVagaDono, pesquisaId);
                     cout << "Voce deseja excluir o(a) " << donos[pesquisaBemSucedida].nome << endl;
@@ -239,6 +248,7 @@ int main()
                     {
                         cout << pets[posicaoVagaPets].nome << " cadastrado com sucesso\n";
                         system("pause");
+                        posicaoVagaPets++;
                         break;
                     }
                     break;
@@ -246,7 +256,7 @@ int main()
                 case 5:
                 {
                     cout << "ALTERAR PET\n\n";
-
+                    cout << "Digite o id do Pet:\n";
                     pesquisaId = procurarID(idPet);
                     pesquisaBemSucedida = pesquisaIdPet(pets, posicaoVagaPets, pesquisaId);
 
@@ -290,6 +300,7 @@ int main()
                 case 6:
                 {
                     cout << "EXCLUIR PET\n\n";
+                    cout << "Digite o id do Pet:\n";
                     pesquisaId = procurarID(idPet);
                     pesquisaBemSucedida = pesquisaIdPet(pets, posicaoVagaPets, pesquisaId);
 
@@ -330,9 +341,10 @@ int main()
                 cout << "2. Consultar pets no sistema\n";
                 cout << "3. Mostrar relatorio do pet\n";
                 cout << "4. Mostrar todos os relatorios\n";
-                cout << "5. Alterar relatorio\n";
-                cout << "6. Excluir relatorio\n";
-                cout << "7. Retornar ao menu anterior\n";
+                cout << "5. Adicionar relatorio\n";
+                cout << "6. Alterar relatorio\n";
+                cout << "7. Excluir relatorio\n";
+                cout << "8. Retornar ao menu anterior\n";
                 cout << "   Digite uma opcao: \n";
                 cin >> subMenu;
 
@@ -348,19 +360,126 @@ int main()
 
                     break;
                 }
-                case 2: {
+                case 2:
+                {
                     system("cls");
                     cout << "CONSULTAR PETS NO SISTEMA\n\n";
 
-                    mostrarPets(posicaoVagaPets,pets,donos, pesquisaId);
+                    mostrarPets(posicaoVagaPets, pets, donos, pesquisaId);
                     system("pause");
+                    break;
+                }
+                case 3:
+                {
+                    system("cls");
+                    cout << "MOSTRAR RELATORIO DE PET\n\n";
+                    cout << "Digite o id do Pet:\n";
+                    pesquisaId = procurarID(idPet);
+                    system("cls");
+                    mostrarRelatorioPet(cadastros, pets, pesquisaId, posicaoVagaCadastro);
+                    system("pause");
+                    break;
+                }
+                case 4:
+                {
+                    system("cls");
+                    cout << "MOSTRAR TODOS OS RELATORIOS\n\n";
+                    mostrarTodosRelatorios(cadastros, pets, posicaoVagaCadastro);
+                    system("pause");
+                    break;
+                }
+                case 5:
+                {
+                    system("cls");
+                    cout << "CADASTRAR RELATORIO\n\n";
+                    cout << "Digite o id do pet que deseja fazer um relatorio\n";
+                    pesquisaId = procurarID(idPet);
+                    cadastros[posicaoVagaCadastro] = cadastrarRelatorio(queixa, diagnostico, tratamento, dataHora, pesquisaId, idCadastro);
+                    system("pause");
+                    break;
+                }
+                case 6:
+                {
+                    system("cls");
+                    cout << "ALTERAR RELATORIO\n\n";
+                    cout << "Digite o id do Relatorio:\n";
+                    pesquisaId = procurarID(idCadastro);
+                    pesquisaBemSucedida = pesquisaIdRelatorio(cadastros, posicaoVagaCadastro, pesquisaId);
+
+                    if (pesquisaBemSucedida != -1)
+                    {
+                        cout << "Voce deseja alterar o relatorio de " << pets[pesquisaBemSucedida].nome << endl;
+                        cout << "Digite 's' para confirmar\n";
+                        cin >> confirmacao;
+                        if (confirmacao == 's')
+                        {
+                            system("cls");
+                            cout << "Digite a queixa do paciente\n";
+                            cin >> cadastros[pesquisaBemSucedida].queixa;
+                            cout << "Diga o diagnostico do veterinario\n";
+                            cin >> cadastros[pesquisaBemSucedida].diagnostico;
+                            cout << "Diga o tratamento\n";
+                            cin >> cadastros[pesquisaBemSucedida].tratamento;
+                            cout << "Diga a data e a hora que esta acontecendo. EX(00/00/00 as 00:00)\n";
+                            cin >> cadastros[pesquisaBemSucedida].dataHora;
+                            cout << endl;
+                            system("pause");
+                            system("cls");
+                            cout << "Relatorio alterado com sucesso!\n";
+                            system("pause");
+                        }
+                    }
+                    else
+                    {
+                        cout << "Id nao encontrado\n";
+                        system("pause");
+                    }
+
+                    break;
+                }
+                case 7:
+                {
+                    system("cls");
+                    cout << "EXCLUIR RELATORIO\n\n";
+                    cout << "Digite o id do Relatorio:\n";
+                    pesquisaId = procurarID(idCadastro);
+                    pesquisaBemSucedida = pesquisaIdRelatorio(cadastros, posicaoVagaCadastro, pesquisaId);
+
+                    if (pesquisaBemSucedida != -1)
+                    {
+                        cout << "Voce deseja excluir o relatorio de " << pets[pesquisaBemSucedida].nome << endl;
+                        cout << "Digite 's' para confirmar\n";
+                        cin >> confirmacao;
+                        if (confirmacao == 's')
+                        {
+                            system("cls");
+                            cadastros[pesquisaBemSucedida].queixa = "relatorio excluido";
+                            cadastros[pesquisaBemSucedida].tratamento = "relatorio excluido";
+                            cadastros[pesquisaBemSucedida].diagnostico = "relatorio excluido";
+                            cadastros[pesquisaBemSucedida].dataHora = "relatorio excluido";
+                            cadastros[pesquisaBemSucedida].id = -1;
+                            cadastros[pesquisaBemSucedida].idPet = -1;
+
+                            cout << endl;
+                            system("pause");
+                            system("cls");
+                            cout << "Relatorio excluido com sucesso!\n";
+                            system("pause");
+                        }
+                    }
+                    else
+                    {
+                        cout << "Id nao encontrado\n";
+                        system("pause");
+                    }
+
                     break;
                 }
 
                 break;
                 }
 
-            } while (subMenu != 7);
+            } while (subMenu != 8);
             break;
         }
         break;
@@ -402,6 +521,18 @@ int pesquisaIdPet(Pet *pets, int posicaoVagaPet, int idPet)
     for (int i = 0; i < posicaoVagaPet; i++)
     {
         if (pets[i].id == idPet)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int pesquisaIdRelatorio(Cadastro *cadastro, int posicaoVagaCadastro, int idCadastro)
+{
+    for (int i = 0; i < posicaoVagaCadastro; i++)
+    {
+        if (cadastro[i].id == idCadastro)
         {
             return i;
         }
@@ -454,11 +585,11 @@ void mostrarDonos(int posicaoVagaDono, Donos *donos)
 {
     system("cls");
     cout << "DADOS DO DONO CADASTRADO:\n\n";
-    cout << " __________________   "
-            " ___________       "
-            "_________________ "
+    cout << " _______________   "
+            "      _______    "
+            "     ____________"
          << endl;
-    cout << "|      NOME:      |    |   ID      |     |   TELEFONE      |"
+    cout << "|    NOME       |\t|  ID   |\t|  TELEFONE  |\t"
          << "\n";
 
     for (int i = 0; i < posicaoVagaDono; i++)
@@ -466,9 +597,9 @@ void mostrarDonos(int posicaoVagaDono, Donos *donos)
     {
         if (donos[i].id != 0)
         {
-            cout << "|" << donos[i].nome << "          |";
-            cout << "   |   " << donos[i].id << "          |";
-            cout << "     |" << donos[i].telefone << "      |" << endl;
+            cout << "|   " << donos[i].nome << "\t|";
+            cout << "       |  " << donos[i].id << "\t|  ";
+            cout << "     |  " << donos[i].telefone << " |\t" << endl;
         }
     }
 }
@@ -476,30 +607,23 @@ void mostrarDonos(int posicaoVagaDono, Donos *donos)
 void mostrarPets(int posicaoVagaPet, Pet *pets, Donos *donos, int idDono)
 {
     system("cls");
-    cout << "DADOS DO DONO CADASTRADO:\n\n";
-    cout << " __________________   "
-            " ___________     "
-            "_______ "
-            "      _____________   "
-            " _______"
-            " _________________"
-         << endl;
-    cout << "|      NOME      |    |   DONO   |     |IDPET |    |   ESPECIE   |   | SEXO |   |  PESO   |   |  PORTE  |"
-         << "\n";
+    cout << "DADOS DOS PETS CADASTRADOS:\n\n";
 
-    for (int i = 0; i < posicaoVagaPet; i++)
+    cout << "|  NOME   \t|  |   DONO  \t|  |  IDPET \t|  |  ESPECIE  \t|  |  SEXO  |\t|  PESO  |\t|  PORTE |\t\n";
+
+    for (int i = 0; i <= posicaoVagaPet; i++)
 
     {
         idDono = pets[i].idDono;
         if (pets[i].id != 0)
         {
-            cout << "|" << pets[i].nome << "          |";
-            cout << "    |   " << donos[idDono].nome << "   |";
-            cout << "      |  " << pets[i].id << " |";
-            cout << "     |   " << pets[i].especie << " |";
-            cout << "     |      " << pets[i].sexo << " |";
-            cout << "   | " << pets[i].peso << "  |";
-            cout << "     |" << pets[i].porte << "      |" << endl;
+            cout << "|  " << pets[i].nome << "  \t|";
+            cout << "  |  " << donos[idDono].nome << " \t|";
+            cout << "  |  " << pets[i].id << "  \t|";
+            cout << "  |  " << pets[i].especie << "\t|";
+            cout << "  |  " << pets[i].sexo << "\t    |";
+            cout << "   |  " << pets[i].peso << "\t |";
+            cout << "      | " << pets[i].porte << " |\t" << endl;
         }
     }
 }
@@ -508,7 +632,6 @@ int procurarID(int id)
 {
     do
     {
-        cout << "Digite o id do Dono/Pet:\n";
         cin >> id;
         if (id == 0)
         {
@@ -517,4 +640,59 @@ int procurarID(int id)
         }
         return id;
     } while (id == 0);
+}
+
+void mostrarRelatorioPet(Cadastro *cadastro, Pet *pets, int idPet, int posicaoVagaCadastro)
+{
+    for (int i = 0; i <= posicaoVagaCadastro; i++)
+    {
+        if (idPet == pets[i].id)
+        {
+            cout << "TESTEEE" << i << endl;
+            cout << "|  NOMEPET   \t|  |   QUEIXA  \t\t|  |  DIAGNOSTICO \t\t|  |  TRATAMENTO  \t\t|  |  DATA/HORA  \t\t|\n";
+            cout << "|  " << pets[i].nome << "  \t|";
+            cout << "  |  " << cadastro[i].queixa << " \t|";
+            cout << "  |  " << cadastro[i].diagnostico << "  \t|";
+            cout << "  |  " << cadastro[i].tratamento << "\t|";
+            cout << "  |  " << cadastro[i].dataHora << "\t|\n";
+
+            break;
+        }
+    }
+}
+
+void mostrarTodosRelatorios(Cadastro *cadastro, Pet *pets, int posicaoVagaCadastro)
+{
+
+    for (int i = 0; i <= posicaoVagaCadastro; i++)
+    {
+        cout << "|  NOMEPET   \t|  |   QUEIXA  \t\t|  |  DIAGNOSTICO \t\t|  |  TRATAMENTO  \t\t|  |  DATA/HORA  \t\t|\n";
+        cout << "|  " << pets[i].nome << "  \t|";
+        cout << "  |  " << cadastro[i].queixa << " \t|";
+        cout << "  |  " << cadastro[i].diagnostico << "  \t|";
+        cout << "  |  " << cadastro[i].tratamento << "\t|";
+        cout << "  |  " << cadastro[i].dataHora << "\t|\n";
+        cout << "\n";
+    }
+}
+
+Cadastro cadastrarRelatorio(string queixa, string diagnostico, string tratamento, string dataHora, int idPet, int id)
+{
+    Cadastro cadastro;
+    cadastro.id = id;
+    cadastro.idPet = idPet;
+    cout << "Digite a queixa do paciente\n";
+    cin >> queixa;
+    cadastro.queixa = queixa;
+    cout << "Diga o diagnostico do veterinario\n";
+    cin >> diagnostico;
+    cadastro.diagnostico = diagnostico;
+    cout << "Diga o tratamento\n";
+    cin >> tratamento;
+    cadastro.tratamento = tratamento;
+    cout << "Diga a data e a hora que esta acontecendo. EX(00/00/00 as 00:00)\n";
+    cin >> dataHora;
+    cadastro.dataHora = dataHora;
+    id++;
+    return cadastro;
 }
